fa45ab0f26494c4eda95e0b182432adf10a239c0
Beryllium <francesco.penasa.job@gmail.com>
2025-11-16 14:41:20 +0100

    refactor dir structure

---
+++ esempio/main.lua
@@ -0,0 +1,17 @@
+
+function love.load()
+
+end
+
+function love.update(dt)
+
+end
+
+function love.draw()
+    print('test')
+    print('test2')
+    love.graphics.print("CIAO MONDO! ", 50, 50 ) 
+    love.graphics.print("CIAO MONDO22! ", 50, 80 ) 
+
+end
+
